d73448570dc3d2a0f25c355be5d6a7acd64457e7
T-ragon <49035572+T-ragon@users.noreply.github.com>
2024-07-30 23:21:28 +0800

    dgsdgf

--- modules/core/04-channel/types/codec.go
+++ modules/core/04-channel/types/codec.go
@@ -50,8 +50,7 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 		&MsgPruneAcknowledgements{},
 		&MsgUpdateParams{},
 	)
-
-	fmt.Println(&_Msg_serviceDesc.Methods)
+	
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 	fmt.Println(registry.ListAllInterfaces())
 	_, err := registry.Resolve("/ibc.core.channel.v1.MsgRecvRootHash")
